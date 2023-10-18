e6582537ebb5f1347b97c07041dff04b3f1609ac
Roy-Gao <gzq22@mails.tsinghua.edu.cn>
2023-10-18 15:03:51 -0700

    Update flood fill.py

--- flood fill.py
+++ flood fill.py
@@ -2,10 +2,13 @@ from typing import List
 
 def flood_fill(input_board: List[str], old: str, new: str, x: int, y: int) -> List[str]:
     def fill(x, y):
-        if x < 0 or x >= len(input_board) or y < 0 or y >= len(input_board[0]):
+        # You can combine the two if statements below into one, but I think it's easier to read this way.
+        if x < 0 or x >= len(input_board) or y < 0 or y >= len(input_board[0]) or input_board[x][y] != old:
             return
-        if input_board[x][y] != old:
-            return
+        # if x < 0 or x >= len(input_board) or y < 0 or y >= len(input_board[0]):
+        #     return
+        # if input_board[x][y] != old:
+        #     return
         input_board[x] = input_board[x][:y] + new + input_board[x][y + 1:]
         fill(x - 1, y)
         fill(x + 1, y)
